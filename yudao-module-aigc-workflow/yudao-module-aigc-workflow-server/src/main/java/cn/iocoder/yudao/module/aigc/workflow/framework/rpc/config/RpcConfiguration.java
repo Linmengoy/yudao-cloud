25878fac8b6b3c0a83b7664ba677f7577649afca
linmeng <819991304@qq.com>
2026-06-01 11:42:52 +0800

    feat(aigc-workflow): add AigcAssetApi feign client dependency
    新增AigcAssetApi的Feign客户端依赖，完善工作流模块的RPC调用配置

--- yudao-module-aigc-workflow/yudao-module-aigc-workflow-server/src/main/java/cn/iocoder/yudao/module/aigc/workflow/framework/rpc/config/RpcConfiguration.java
+++ yudao-module-aigc-workflow/yudao-module-aigc-workflow-server/src/main/java/cn/iocoder/yudao/module/aigc/workflow/framework/rpc/config/RpcConfiguration.java
@@ -1,12 +1,13 @@
 package cn.iocoder.yudao.module.aigc.workflow.framework.rpc.config;
 
 import cn.iocoder.yudao.module.aigc.billing.api.AigcBillingApi;
+import cn.iocoder.yudao.module.aigc.asset.api.AigcAssetApi;
 import cn.iocoder.yudao.module.aigc.gen.api.AigcGenerateApi;
 import cn.iocoder.yudao.module.aigc.model.api.AigcModelApi;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration(value = "aigcWorkflowRpcConfiguration", proxyBeanMethods = false)
-@EnableFeignClients(clients = {AigcGenerateApi.class, AigcModelApi.class, AigcBillingApi.class})
+@EnableFeignClients(clients = {AigcGenerateApi.class, AigcModelApi.class, AigcBillingApi.class, AigcAssetApi.class})
 public class RpcConfiguration {
 }
