16fc2aa9ed85fdfaa718b4fbfb38e8af0e2f3ea6
linmeng <819991304@qq.com>
2026-06-09 10:47:05 +0800

    fix(aigc-workflow): add missing FileApi Feign client
    补全AIGC工作流模块的RPC配置，添加文件服务的Feign客户端依赖

--- yudao-module-aigc-workflow/yudao-module-aigc-workflow-server/src/main/java/cn/iocoder/yudao/module/aigc/workflow/framework/rpc/config/RpcConfiguration.java
+++ yudao-module-aigc-workflow/yudao-module-aigc-workflow-server/src/main/java/cn/iocoder/yudao/module/aigc/workflow/framework/rpc/config/RpcConfiguration.java
@@ -4,10 +4,11 @@ import cn.iocoder.yudao.module.aigc.billing.api.AigcBillingApi;
 import cn.iocoder.yudao.module.aigc.asset.api.AigcAssetApi;
 import cn.iocoder.yudao.module.aigc.gen.api.AigcGenerateApi;
 import cn.iocoder.yudao.module.aigc.model.api.AigcModelApi;
+import cn.iocoder.yudao.module.infra.api.file.FileApi;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration(value = "aigcWorkflowRpcConfiguration", proxyBeanMethods = false)
-@EnableFeignClients(clients = {AigcGenerateApi.class, AigcModelApi.class, AigcBillingApi.class, AigcAssetApi.class})
+@EnableFeignClients(clients = {AigcGenerateApi.class, AigcModelApi.class, AigcBillingApi.class, AigcAssetApi.class, FileApi.class})
 public class RpcConfiguration {
 }
